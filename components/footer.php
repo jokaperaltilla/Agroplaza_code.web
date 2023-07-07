0916e4d833912dc0c32e07ddf0dd914b862364b3
jokaperaltilla <42878841+jokaperaltilla@users.noreply.github.com>
2023-07-07 13:18:13 -0500

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,38 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="imagen/email-icon.png" alt="">
+         <h3>our email</h3>
+         <a href="mailto:wilber16119@gmail.com">peraltillajosecarlos@gmail.com</a>
+      </div>
+
+      <div class="box">
+         <img src="imagen/clock-icon.png" alt="">
+         <h3>opening hours</h3>
+         <p>08:00am to 18:00pm</p>
+      </div>
+
+      <div class="box">
+         <img src="imagen/map-icon.png" alt="">
+         <h3>our address</h3>
+         <a href="#">Calle misti 41# miraflores</a>
+      </div>
+
+      <div class="box">
+         <img src="imagen/phone-icon.png" alt="">
+         <h3>our number</h3>
+         <a href="tel:+51983495557">983-495-557</a>
+      </div>
+
+   </section>
+
+</footer>
+
+<div class="loader">
+   <img src="imagen/dis1.gif" alt="">
+</div>
+<!-- <div class="loader">
+   <img src="images/loader.gif" alt="">
+</div> -->
